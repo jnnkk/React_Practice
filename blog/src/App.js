e0113141680120ce19fef545ed4b0e329adf15bd
SJY <jun000405@naver.com>
2023-12-20 14:32:53 +0900

    임시저장

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 고기 맛집';
+  let [body_title, b] = useState(['남자 코트 추천', '여자 코트 추천', '강남 우동 맛집']);
+  // useState는 내용이 바뀌면 알아서 자동 랜더링 해줌
+  // destructuring 문법
+  let [thumb, thumb_change] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{post}</h4>
+      </div>
+      <div className="list">
+        <h4>{ body_title[0] } <span onClick={()=>{ thumb_change(thumb+1) }}>👍</span> {thumb} </h4>
+        <p>12월 20일</p>
+      </div>
+      <div className="list">
+        <h4>{ body_title[1] }</h4>
+        <p>12월 20일</p>
+      </div>
+      <div className="list">
+        <h4>{ body_title[2] }</h4>
+        <p>12월 20일</p>
+      </div>
     </div>
   );
 }
+
+// class 는 className 으로 써야함
+// {} 안에는 변수를 넣을 수 있음
+// style={{color: 'blue'}} 이런식으로 쓸 수 있음
 
 export default App;
